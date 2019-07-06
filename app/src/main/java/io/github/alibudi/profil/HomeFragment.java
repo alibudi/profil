74f94bc34be8f89ec8db567b36f1c30c8029c3b1
alibudi <alibudipurnomo@gmail.com>
2019-07-06 17:32:24 +0700

    profil

---
+++ app/src/main/java/io/github/alibudi/profil/HomeFragment.java
@@ -0,0 +1,42 @@
+package io.github.alibudi.profil;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    ImageView ivLogo;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+       View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+       ivLogo = view.findViewById(R.id.iv_logo);
+
+        ivLogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Test Toast", Toast.LENGTH_SHORT).show();
+            }
+        });
+       return view;
+    }
+
+}
